ca31a902b234a8e8d0d100367b43f5b582b78e6b
Wahaha <b0464221@gmail.com>
2022-06-03 22:15:00 +0700

    Add basic event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express(); // This program is really like echo broadcaster !
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body; // Can be any data type
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: 'OK' }); // Tell the  original event sender OK
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
